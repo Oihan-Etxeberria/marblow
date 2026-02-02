91d1901937e9df07ff6b81740423b7241112b1aa
Oihan Etxeberria <oetxeberriaza24wg@ikzubirimanteo.com>
2026-02-02 11:26:56 +0100

    cambiar el vite config y añadir roles a User

--- vite.config.js
+++ vite.config.js
@@ -11,13 +11,12 @@ export default defineConfig({
         react(),
     ],
     server: {
-        // ¡Importante! Permite que Vite escuche en todas las interfaces
-        host: '0.0.0.0',          // o '::' si usas IPv6
+        host: '0.0.0.0',
         port: 5173,
-
-        // Configuración CORS explícita y segura
         cors: true,
-        origin: 'http://10.14.0.111:5173', // o mejor: la IP que uses desde fuera
-        // origin: 'http://192.168.1.100:5173', // ← pon aquí la IP que uses en el navegador cliente
+
+        hmr: {
+            host: 'localhost',
+        },
     },
 });
